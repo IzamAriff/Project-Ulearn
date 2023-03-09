3a5bf36712626410ace38dd82812115d7fee4f5b
DrMuddin <126545245+DrMuddin@users.noreply.github.com>
2023-03-10 00:55:07 +0800

    Add files via upload

---
+++ student.java.java
@@ -0,0 +1,43 @@
+public class student{
+    
+    String programme;
+    private course[] = Course;
+    private fee Fee;
+    private semester Semester;
+    
+    public addCourse(couse Course) {
+        return course;
+    }
+    public dropCourse(course Course){
+        return course;
+    }
+
+    void getCourse() {
+        return 0;
+    }
+
+    void getProgramme() {
+        return 0;
+    }
+
+    public void setProgramme(String programe) {
+        this.programme = programme;
+    }
+       
+    void getCourse() {
+        return 0;
+    }
+    
+    public void setFee(fee Fee) {
+        this.fee = fee;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
