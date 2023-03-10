b99f1ec34233b09a9a8d2e45dea1d6bb82f03fc1
Izam Ariff <106332727+IzamAriff@users.noreply.github.com>
2023-03-10 17:44:25 +0800

    adding constructor
    along with few changes

--- Student.java
+++ Student.java
@@ -1,9 +1,22 @@
-public class student{
+public class Student extends Person {
     
-    String programme;
+    //attributes
+    private String programme;
     private course[] = Course;
     private fee Fee;
     private semester Semester;
+    
+    //constructor: assigning values to attributes
+    public Student(String name, String id, int age, String ic, String department,
+                   Course[] courses, String programme, Fee fee, Semester semester) {
+        
+        // call the constructor of the Person class using 'super'
+        super(name, id, age, ic, department); 
+        this.courses = courses;
+        this.programme = programme;
+        this.fee = fee;
+        this.semester = semester;
+    }
     
     public addCourse(couse Course) {
         return course;
