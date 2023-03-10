3f0a3a479fed8b599623d2757025abf64fc5bea8
SaifullahSaz <126546010+SaifullahSaz@users.noreply.github.com>
2023-03-10 22:17:05 +0800

    Add files via upload
    sai hensem

---
+++ Registration.java
@@ -0,0 +1,14 @@
+public class registration {
+    private Student student;
+
+    public void addCourse() {
+
+    }
+
+    public void dropCourse() {
+        
+    }
+    public void logIn(String username, String id) {
+
+    }
+}
