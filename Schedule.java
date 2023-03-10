4573b0738b008612d6e4b7eca234ba11c5c578bb
Izam Ariff <106332727+IzamAriff@users.noreply.github.com>
2023-03-10 22:32:10 +0800

    Create Schedule class

---
+++ Schedule.java
@@ -0,0 +1,22 @@
+public class Schedule {
+  
+  //attribute
+  private Session[] session;
+  
+  //constructor
+  private Schedule(Session[] session) {
+    this.session = session;
+  }
+  
+  //setter and getter
+  public void setSession(Session session) {
+    this.session = session;
+  }
+  
+  public Session getSession() {
+    return session;
+  }
+  
+  //method
+  public void viewSchedule(Session session[]) {
+  }
