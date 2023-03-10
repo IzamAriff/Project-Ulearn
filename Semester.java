869f03b362e7834ef462c2bb71729ab2172a1c75
Izam Ariff <106332727+IzamAriff@users.noreply.github.com>
2023-03-10 21:32:47 +0800

    Created Semeste class
    by irfan

---
+++ Semester.java
@@ -0,0 +1,27 @@
+
+public class Semester {
+
+    //attribute
+    private String name;
+    private char[][] grades;
+    
+    //constructor
+    public Semester (String
+    
+    //getter and setter
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public char[][] getGrades() {
+        return grades;
+    }
+    
+    public void setGrades(char[][] grades) {
+        this.grades = grades;
+    }
+}
