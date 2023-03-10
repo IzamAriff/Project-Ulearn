fa10c8a74a22437ceae362515c4f24e6901f15f8
DrMuddin <126545245+DrMuddin@users.noreply.github.com>
2023-03-10 22:47:20 +0800

    Add files via upload

---
+++ Fee.java
@@ -0,0 +1,42 @@
+public class fee{
+    
+
+    //attribute
+    private float amount;
+    private float paid;
+  
+
+        //constructor
+    public Course(float amount, float paid) {
+        this.amount = amount;
+        this.paid = paid; 
+    }
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+    
+    public float getPaid() {
+        return paid;
+    }
+
+    public void setPaid(float paid) {
+        this.paid = paid;
+    }
+    
+    public float printInvoice(float amount, float paid){
+        
+        return amount;
+        return paid
+        
+    }
+
+    
+    
+    
+    
+    
+}
