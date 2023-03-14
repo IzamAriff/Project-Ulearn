6118debfdfca56b3eb0407e9d08334234d465a4d
Pokpo404 <80873279+Pokpo404@users.noreply.github.com>
2023-03-14 21:56:47 +0800

    Add files via upload

---
+++ Session.java
@@ -0,0 +1,23 @@
+
+public class Session {
+	
+	private String scheduleID;
+	private String location;
+	private Date date;;
+	private Student[] Students;
+	private Lecturer lecturer;
+	private Course course;
+	
+	
+	public 
+	public void setDate(Date) {
+		this.date = date;
+	}
+
+	public void getSchedule() {
+		System.out.println("Class: " + scheduleID);
+		System.out.println("Date: " + date.printDate());
+		System.out.println("Location: " + location);
+		System.out.println("Lecturer: " + lecturer);
+	}
+}
