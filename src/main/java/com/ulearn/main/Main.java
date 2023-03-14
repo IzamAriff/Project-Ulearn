4462e3d17a39052617db9b682dd36758ea5ab311
Izam Ariff <106332727+IzamAriff@users.noreply.github.com>
2023-03-14 23:55:50 +0800

    Update Main.java

--- src/main/java/com/ulearn/main/Main.java
+++ src/main/java/com/ulearn/main/Main.java
@@ -7,23 +7,23 @@ public class Main {
     public static void main(String[] args) throws SQLException {
 
         /*
-        // create an instance of the StudentDAO class
-        StudentDAO studentDAO = new StudentDAO();
+        // create an instance of the StudentDataAccess class
+        StudentDataAccess studentData = new StudentDataAccess();
 
         // create a new student record in the database
         Student newStudent = new Student("John Doe", "12345", 21, "IC123456", "Computer and Information Science", "john.doe@example.com", "Computer Science");
-        studentDAO.addStudent(newStudent);
+        StudentDataAccess.addStudent(newStudent);
 
         // retrieve a student record from the database
-        Student retrievedStudent = studentDAO.getStudentById("12345");
+        Student retrievedStudent = StudentDataAccess.getStudentById("12345");
         System.out.println("Retrieved student: " + retrievedStudent);
 
         // update a student record in the database
         retrievedStudent.setAge(22);
-        studentDAO.updateStudent(retrievedStudent);
+        StudentDataAccess.updateStudent(retrievedStudent);
 
         // delete a student record from the database
-        studentDAO.deleteStudent("12345");
+        StudentDataAccess.deleteStudent("12345");
 
          */
 
